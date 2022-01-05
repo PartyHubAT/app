917033df572488188983c04e9fd5382acb5961f9
ComradeVanti <kingvanti650@gmail.com>
2022-01-05 18:47:19 +0100

    Add home-page html
    Dead html structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,22 @@
 ﻿<template>
-  <div></div>
+  <div>
+    <h1>PH</h1>
+    <h2>Home</h2>
+    <div>
+      <label>
+        Name
+        <input />
+      </label>
+    </div>
+    <button>Host</button>
+    <div>
+      <label>
+        Room-id
+        <input type="number" />
+      </label>
+    </div>
+    <button>Join</button>
+  </div>
 </template>
 
 <script>
