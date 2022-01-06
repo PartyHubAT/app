aaf0ea06138b597b4ee60ecce1758eac972dea71
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 08:42:55 +0100

    Add logic to display room-id in lobby

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,8 +1,18 @@
-﻿<template><div></div></template>
+﻿<template>
+  <div>
+    <h1>PH</h1>
+    <h2>Lobby {{ roomId }}</h2>
+  </div>
+</template>
 
 <script>
 export default {
   name: "Lobby",
+  computed: {
+    roomId() {
+      return this.$store.state.roomId;
+    },
+  },
 };
 </script>
 
