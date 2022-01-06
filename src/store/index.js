206639ad4f4ed30faf4e20f9ed37aab6546915c1
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 08:25:59 +0100

    Add room-id to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    roomId: 0,
+  },
+  mutations: {
+    joinRoom(state, id) {
+      state.roomId = id;
+    },
+  },
   actions: {},
   modules: {},
 });
