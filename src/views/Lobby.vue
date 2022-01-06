0ea87b3a1166185d3a80e03f4b6ee1d0e1bcf4f5
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 08:43:27 +0100

    Add logic to display player names in lobby

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -2,12 +2,23 @@
   <div>
     <h1>PH</h1>
     <h2>Lobby {{ roomId }}</h2>
+    <div>
+      <h3>Players</h3>
+      <ul>
+        <li v-for="player in players" :key="player.name">{{ player.name }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Lobby",
+  data() {
+    return {
+      players: [],
+    };
+  },
   computed: {
     roomId() {
       return this.$store.state.roomId;
