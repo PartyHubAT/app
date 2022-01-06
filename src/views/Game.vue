f85cfbca870817689d14e1dce22d2b568ca1099f
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 10:10:29 +0100

    Add game-page

---
+++ src/views/Game.vue
@@ -0,0 +1,31 @@
+﻿<template>
+  <div id="page">
+    <h1>PH</h1>
+    <iframe ref="gameContainer" id="gameContainer" :src="gameUrl"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Game",
+  computed: {
+    gameUrl() {
+      return `http://${window.location.host}/game/${this.$store.state.gameName}/index.html`;
+    },
+  },
+  mounted() {
+    this.$refs.gameContainer.contentWindow.socket = this.$socket;
+  },
+};
+</script>
+
+<style scoped>
+#page {
+  display: flex;
+  flex-direction: column;
+  height: 95vh;
+}
+#gameContainer {
+  flex-grow: 1;
+}
+</style>
