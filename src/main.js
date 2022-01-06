f9fb4a7b4a126267965d24b9a413168ec5f32d23
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 09:47:38 +0100

    Add axios

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import VueSocketIO from "vue-3-socket.io";
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(VueAxios, axios)
   .use(
     new VueSocketIO({
       debug: true,
