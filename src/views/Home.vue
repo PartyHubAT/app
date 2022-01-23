574a0993eca778a6b8d253573ef7fc7de8319a49
lukasganster <lukasganster13@gmail.com>
2022-01-23 15:22:25 +0100

    Add basic tailwind for home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,70 @@
 ﻿<template>
-  <div>
-    <h1>PH {{ $route.params.id }}</h1>
-    <h2>Home</h2>
-    <div>
-      <label> Name </label>
-      <div :class="{ error: v$.playerName.$errors.length }">
-        <input
-          v-model="playerName"
-          required
-          maxlength="64"
-          minlength="1"
-          ref="playerNameInput"
-        />
-        <div
-          class="input-errors"
-          v-for="error of v$.playerName.$errors"
-          :key="error.$uid"
-        >
-          <div class="error-msg">Enter a valid player name</div>
+  <div class="flex justify-center">
+    <div class="mb-3 xl:w-96">
+      <h1 class="text-5xl font-medium leading-tight mt-0 mb-2 text-blue-600">
+        PartyHub {{ $route.params.id }}
+      </h1>
+      <h2 class="text-3xl font-medium leading-tight mt-0 mb-2 text-blue-600">
+        Home
+      </h2>
+      <div>
+        <label class="form-label inline-block mb-2 text-gray-700 text-xl">
+          Name
+        </label>
+        <div :class="{ error: v$.playerName.$errors.length }">
+          <input
+            v-model="playerName"
+            required
+            maxlength="64"
+            minlength="1"
+            ref="playerNameInput"
+            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          />
+          <div
+            class="input-errors"
+            v-for="error of v$.playerName.$errors"
+            :key="error.$uid"
+          >
+            <div class="error-msg">Enter a valid player name</div>
+          </div>
         </div>
       </div>
-    </div>
-    <button @click="host" :disabled="playerName == ''">Host</button>
-    <div>
-      <label> Room-id </label>
-      <div :class="{ error: v$.roomId.$errors.length }">
-        <input
-          v-model="roomId"
-          type="text"
-          pattern="^\d{1,6}$"
-          placeholder="123456"
-        />
-        <div
-          class="input-errors"
-          v-for="error of v$.roomId.$errors"
-          :key="error.$uid"
-        >
-          <div class="error-msg">Enter a six digit room numbers</div>
+      <button
+        @click="host"
+        :disabled="playerName == ''"
+        class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+      >
+        Host
+      </button>
+      <div>
+        <label class="form-label inline-block mb-2 text-gray-700 text-xl">
+          Room-id
+        </label>
+        <div :class="{ error: v$.roomId.$errors.length }">
+          <input
+            v-model="roomId"
+            type="text"
+            pattern="^\d{1,6}$"
+            placeholder="123456"
+            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          />
+          <div
+            class="input-errors"
+            v-for="error of v$.roomId.$errors"
+            :key="error.$uid"
+          >
+            <div class="error-msg">Enter a six digit room numbers</div>
+          </div>
         </div>
       </div>
+      <button
+        @click="join"
+        :disabled="playerName == '' || roomId == ''"
+        class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+      >
+        Join
+      </button>
     </div>
-    <button @click="join" :disabled="playerName == '' || roomId == ''">
-      Join
-    </button>
   </div>
 </template>
 
