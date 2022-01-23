7b18089cbf0286ebbc710c08cd9e84ba5f56f391
lukasganster <lukasganster13@gmail.com>
2022-01-23 19:21:44 +0100

    Update Settings.vue

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>settings</div>
+  <div style="border: 4px solid red" v-if="settings">
+    <h3>Settings</h3>
+    <form>
+      <div
+        v-for="(element, name) in settings.form"
+        :key="name"
+        style="border: 1px solid blue"
+      >
+        <label>{{ element.name }}</label>
+        <label>{{ element.label }}</label>
+        <input
+          v-if="element.type == 'Number'"
+          type="number"
+          :min="element.min"
+          :max="element.max"
+          :required="element.required"
+        />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
