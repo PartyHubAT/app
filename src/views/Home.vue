172c2e1ac85960eb82188d5d01c8e225638553e5
lukasganster <lukasganster13@gmail.com>
2022-01-23 16:47:16 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,7 +32,7 @@
       <button
         @click="host"
         :disabled="playerName == ''"
-        class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        class="inline-block px-6 w-full py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
       >
         Host
       </button>
@@ -60,7 +60,7 @@
       <button
         @click="join"
         :disabled="playerName == '' || roomId == ''"
-        class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        class="inline-block w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
       >
         Join
       </button>
