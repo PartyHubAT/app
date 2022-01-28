d87cae4a9f4c33a1cb31356af10e0936626b3ed2
lukasganster <lukasganster13@gmail.com>
2022-01-28 12:42:01 +0100

    Update Settings component

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -15,8 +15,39 @@
             :min="element.min"
             :max="element.max"
             :required="element.required"
-            v-model="element.defaultValue"
+            v-model="element.value"
           />
+        </div>
+        <div v-if="element.type == 'Text' || element.type == 'String'">
+          {{ element.name }}
+          <label>{{ element.label }}</label>
+          <input
+            type="text"
+            :required="element.required"
+            v-model="element.value"
+          />
+        </div>
+        <div v-if="element.type == 'Checkbox'">
+          {{ element.name }}
+          <label>{{ element.label }}</label>
+          <input
+            type="checkbox"
+            :required="element.required"
+            v-model="element.value"
+          />
+        </div>
+        <div v-if="element.type == 'Select'">
+          {{ element.name }}
+          <label>{{ element.label }}</label>
+          <select v-model="element.value" :multiple="element.multiple">
+            <option
+              v-for="option in element.options"
+              :key="option"
+              :value="option"
+            >
+              {{ option }}
+            </option>
+          </select>
         </div>
       </div>
     </form>
@@ -31,7 +62,20 @@ export default {
       type: Object,
     },
   },
-  methods: {},
+  watch: {
+    settings: {
+      handler: function () {
+        this.$store.commit("gameSettings", this.settings);
+        console.log("settings updated");
+        this.$root.gameSettings = this.settings;
+        // whenever the car price changes, this function will be executed
+      },
+      deep: true,
+    },
+  },
+  mounted() {
+    this.$store.commit("gameSettings", this.settings);
+  },
 };
 </script>
 
